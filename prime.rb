b825bcb346eb6631638ff75bb621c0046c2a74aa
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-20 02:22:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,15 @@
 require 'prime'
 
 def prime?(n)
-  n = 2 
+
  start = 2
   if number > 1
     range = (start..number-1).to_a
    if range.prime? 
      true 
+   else 
+     false 
    end 
  end
- else 
-     false 
  end 
     
